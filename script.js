34f72d279cc221a1b0c3caaa1df183626d3ed60a
WURST1one <henrique.marchini.souza@gmail.com>
2025-01-12 00:12:18 -0300

    By me a coffe

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
+const githubLink = document.querySelector(".github-link");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "https://github.com/WURST1one";
+  question.innerHTML = 'Check out my GitHub: <a href="https://github.com/WURST1one" target="_blank" class="animated-link">https://github.com/WURST1one</a>';
   gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  
+  // Add an animation when the link appears
+  const link = document.querySelector(".animated-link");
+  link.style.animation = "fadeIn 1s ease-in-out";
 });
 
-
+// Make the No button move within bounds when hovered
 noBtn.addEventListener("mouseover", () => {
   const wrapper = document.querySelector(".wrapper");
   const wrapperRect = wrapper.getBoundingClientRect();
   const noBtnRect = noBtn.getBoundingClientRect();
 
-
-  const maxX = wrapperRect.width - noBtnRect.width;
-  const maxY = wrapperRect.height - noBtnRect.height;
+  const maxX = wrapperRect.width - noBtnRect.width - 10; // 10px padding
+  const maxY = wrapperRect.height - noBtnRect.height - 10; // 10px padding
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
